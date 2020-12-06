69b241539ce45ae8c8ca1907de9e2a3bfae8c265
yosoyuva <mehdiyuva@outlook.fr>
2020-12-06 18:47:30 +0100

    main test guacamole

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "inc/ft_printf.h"
+
+#define BPRINT(x, ...) PRINT("\"%s\" // 1st '*' = %d, 2nd '*' = %d\n", x, a, b)
+
+int main()
+{
+//void *s;
+  //ft_printf(" --- Return : %d\n", ft_printf("%-2s, %.s, %-4s, %-2.4s, %-8.12s, %3s, %8s, %---2s, %.*s, %.0s, %.1s, %.2s, %.4s, %.8s", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, -2, NULL, NULL, NULL, NULL, NULL, NULL));
+  //printf(" --- Return : %d\n", printf("%-2s, %.s, %-4s, %-2.4s, %-8.12s, %3s, %8s, %---2s, %.*s, %.0s, %.1s, %.2s, %.4s, %.8s", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, -2, NULL, NULL, NULL, NULL, NULL, NULL));
+
+  //ft_printf("bonjour %s\n", "");
+  //printf("bonjour %s\n", "");
+  //printf("%d\n", sizeof(s));
+  ft_printf("d=%d, x=%x, X=%X, u=%u", -12, -12, -12, -12);
+}
+/*to test : make re && gcc -o test/myprog test/main.c  -L. -lftprintf */
+/* to test leaks : valgrind --tool=memcheck ./myprog  */
