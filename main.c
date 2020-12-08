831dee53b3bde10f2a210c3da3f772ddea971c1c
yosoyuva <mehdiyuva@outlook.fr>
2020-12-08 13:15:36 +0100

    gucamole tests

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "inc/ft_printf.h"
+
+#define BPRINT(x, ...) PRINT("\"%s\" // 1st '*' = %d, 2nd '*' = %d\n", x, a, b)
+
+int main()
+{
+    int		a = -4;
+	int		b = 0;
+	char	c = 'a';
+	int		d = 2147483647;
+	int		e = -2147483648;
+	int		f = 42;
+	int		g = 25;
+	int		h = 4200;
+	int		i = 8;
+	int		j = -12;
+	int		k = 123456789;
+	int		l = 0;
+	int		m = -12345678;
+	char	*n = "abcdefghijklmnop";
+	char	*o = "-a";
+	char	*p = "-12";
+	char	*q = "0";
+	char	*r = "%%";
+	char	*s = "-2147483648";
+	char	*t = "0x12345678";
+	char	*u = "-0";
+
+//void *s;
+  //ft_printf(" --- Return : %d\n", ft_printf("%-2s, %.s, %-4s, %-2.4s, %-8.12s, %3s, %8s, %---2s, %.*s, %.0s, %.1s, %.2s, %.4s, %.8s", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, -2, NULL, NULL, NULL, NULL, NULL, NULL));
+  //printf(" --- Return : %d\n", printf("%-2s, %.s, %-4s, %-2.4s, %-8.12s, %3s, %8s, %---2s, %.*s, %.0s, %.1s, %.2s, %.4s, %.8s", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, -2, NULL, NULL, NULL, NULL, NULL, NULL));
+
+  //ft_printf("bonjour %s\n", "");
+  //printf("bonjour %s\n", "");
+  //printf("%d\n", sizeof(s));
+  //ft_printf(" --- Return : %d\n", ft_printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X", i, j, k, l, m, n, c, c, j, j, j));
+  //printf(" --- Return : %d\n", printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X", i, j, k, l, m, n, c, c, j, j, j));
+
+  ft_printf(" --- Return : %d\n", ft_printf("%c, %-c, %12c, %-3c, %-1c, %1c,%s %-2c, %-4c, %5c, %3c, %-*c, %-*c, %*c, %*c", 0, 0, 0, 0, 0, 0, "bonjour", 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0));
+  printf(" --- Return : %d\n", printf("%c, %-c, %12c, %-3c, %-1c, %1c,%s %-2c, %-4c, %5c, %3c, %-*c, %-*c, %*c, %*c", 0, 0, 0, 0, 0, 0, "bonjour", 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0));
+  //ft_printf("%12c\n", 0);
+  //printf("%12c\n", 0);
+}
+/*to test : make re && gcc -o test/myprog test/main.c  -L. -lftprintf */
+/* to test leaks : valgrind --tool=memcheck ./myprog  */
