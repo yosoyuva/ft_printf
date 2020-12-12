06cf231c8ac7d0ea00eabaa15162e76f757674af
yosoyuva <mehdiyuva@outlook.fr>
2020-12-12 22:16:01 +0100

    725 pft guacamole

--- lib/ft_strnew_space.c
+++ lib/ft_strnew_space.c
@@ -6,7 +6,7 @@
 /*   By: ymehdi <ymehdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 14:47:16 by ymehdi            #+#    #+#             */
-/*   Updated: 2020/12/08 14:45:32 by ymehdi           ###   ########.fr       */
+/*   Updated: 2020/12/12 22:07:57 by ymehdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 char	*ft_strnew_space(size_t size)
 {
-	void	*s;
+	char	*s;
 
-	if ((s = malloc(size + 1)) == NULL)
+	if ((s = malloc(sizeof(s) * (size + 2))) == NULL)
 		return (NULL);
 	ft_memset(s, ' ', size + 1);
+	s[size + 1] = '\0';
 	return (s);
 }
 
 char	*ft_strnew_space_f(char *str, size_t size)
 {
-	void	*s;
+	char	*s;
 
-	if ((s = malloc(size + 1)) == NULL)
+	if ((s = malloc(sizeof(s) * (size + 2))) == NULL)
 		return (NULL);
 	ft_memset(s, ' ', size + 1);
 	free(str);
+	s[size + 1] = '\0';
 	return (s);
 }
