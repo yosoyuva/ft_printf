def622d42f83845b4bfde83ec4390b6ebc3e5d31
Yuva MEHDI <ymehdi@e2r10p13.42.fr>
2020-12-14 15:32:49 +0100

    724/745

--- src/ft_printf_percent.c
+++ src/ft_printf_percent.c
@@ -6,7 +6,7 @@
 /*   By: ymehdi <ymehdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 06:04:28 by ymehdi            #+#    #+#             */
-/*   Updated: 2020/05/15 01:23:24 by ymehdi           ###   ########.fr       */
+/*   Updated: 2020/12/14 15:31:08 by ymehdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ char		*ft_printf_percent(t_fpt *var)
 	if (var->zero)
 		var->zero = var->zero;
 	c = ft_add_c_to_end_of_s(c, '%');
-	ft_init_flag(var);
+	//ft_init_flag(var);
 	return (c);
 }
