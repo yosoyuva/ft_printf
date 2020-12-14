f35e9d2d7db725cbe5b5673bc85089ab701a79b0
Yuva MEHDI <ymehdi@e2r10p13.42.fr>
2020-12-14 16:59:08 +0100

    743/745 + 514/514 with norme

--- src/ft_printf_p.c
+++ src/ft_printf_p.c
@@ -6,7 +6,7 @@
 /*   By: ymehdi <ymehdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 06:00:38 by ymehdi            #+#    #+#             */
-/*   Updated: 2020/12/14 16:33:00 by ymehdi           ###   ########.fr       */
+/*   Updated: 2020/12/14 16:56:42 by ymehdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,11 @@ char		*ft_printf_p(t_fpt *var)
 {
 	char	*s;
 	char	*arg;
-	//char	nil[1];
 
 	if (var->zero)
 		var->zero = var->zero;
-	//nil[0] = '0';
 	arg = ft_itoa_hex_long_p(va_arg(var->list, long int));
-/*	if (!ft_strcmp(arg, nil))
-	{
-		return (arg);
-	}
-	else*/
-		s = ft_strjoin("0x", arg);
+	s = ft_strjoin("0x", arg);
 	free(arg);
 	return (s);
 }
